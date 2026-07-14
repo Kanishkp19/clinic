0e7f179c595072c616556d55dd9fb647dd89f8eb
Kanishk Pandey <kanishk_pandey@Kanishks-MacBook-Air.local>
2026-07-14 19:03:20 +0530

    fix: pin production build to webpack to match dev CSS output
    next build defaulted to Turbopack while dev used --webpack; the two
    bundlers emit different vendor prefixes and CSS rule order, shifting
    navbar/section positioning and spacing in production. Also fix invalid
    browserslist query.

--- app/layout.jsx
+++ app/layout.jsx
@@ -8,27 +8,8 @@ export default function RootLayout({ children }) {
     <html lang="en-US" dir="ltr">
       <head>
         <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
-        <link
-          rel="apple-touch-icon"
-          sizes="180x180"
-          href="/assets/img/favicons/apple-touch-icon.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="32x32"
-          href="/assets/img/favicons/favicon-32x32.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="16x16"
-          href="/assets/img/favicons/favicon-16x16.png"
-        />
-        <link rel="shortcut icon" type="image/x-icon" href="/assets/img/favicons/favicon.ico" />
-        <link rel="manifest" href="/assets/img/favicons/manifest.json" />
-        <meta name="msapplication-TileImage" content="/assets/img/favicons/mstile-150x150.png" />
-        <meta name="theme-color" content="#ffffff" />
+        <link rel="icon" type="image/png" href="/assets/img/logo.png" />
+        <meta name="theme-color" content="#0D3B43" />
         <link href="/assets/css/theme.css" rel="stylesheet" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
